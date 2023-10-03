d46f9783e5f2c6159ded05eb22d3da374c4338a4
matthew kettelkamp <mattkettelkamp@gmail.com>
2023-10-03 14:05:29 -0400

    more mouse interaction

---
+++ src/Overlay.jsx
@@ -0,0 +1,131 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { useSpring, animated } from '@react-spring/web';
+import './style.css'
+
+// Modal Overlay ------------------------------------
+export default function Overlay() {
+
+  const [activeModal, setActiveModal] = useState(null);
+
+  const openAboutModal = () => {
+    setActiveModal(activeModal === 'about' ? null : 'about');
+  };
+
+  const openContactModal = () => {
+    setActiveModal(activeModal === 'contact' ? null : 'contact');
+  };
+
+  const animation = useSpring({
+    transform: activeModal === 'about' ? 'translateX(0%)' : 'translateX(0%)',
+    opacity: activeModal === 'about' ? 1 : 0,
+    config: { mass: 2.8, tension: 50, friction: 15 },
+  });
+
+  const contactAnimation = useSpring({
+    transform: activeModal === 'about' ? 'translateY(-100%)' : 'translateY(0%)',
+    opacity: activeModal === 'contact' ? 1 : 0,
+    config: { mass: 2.8, tension: 50, friction: 15 },
+  });
+
+  const pointerEvents = useSpring({
+    pointerEvents: activeModal === 'about' ? 'auto' : 'none',
+  })
+  const contactPointerEvents = useSpring({
+    pointerEvents: activeModal === 'contact' ? 'auto' : 'none',
+  })
+
+  // 2D button interaction on mousemove -------
+  const [springProps, setSpring] = useSpring(() => ({
+    transform: 'translate(0px, 0px)'
+  }));
+  const [springProps2, setSpring2] = useSpring(() => ({
+    transform: 'translate(0px, 0px)'
+  }));
+
+  const aboutButtonRef = useRef(null);
+  const contactButtonRef = useRef(null);
+
+  // Refactored the mouse move handler to handle both buttons
+  const handleMouseMove = (e) => {
+    [aboutButtonRef, contactButtonRef].forEach(buttonRef => {
+      const button = buttonRef.current;
+      const bounds = button.getBoundingClientRect();
+      const mouseX = e.clientX;
+      const mouseY = e.clientY;
+
+      const centerX = bounds.left + bounds.width / 2;
+      const centerY = bounds.top + bounds.height / 2;
+
+      const deltaX = mouseX - centerX;
+      const deltaY = mouseY - centerY;
+
+      const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+      const maxDistance = 1300;
+      const minPullDistance = 50;  // Within this distance, the pull effect is minimal
+
+      if (distance < maxDistance) {
+        // Quadratic decrease in pullAmount as we get closer to the button
+        const pullAmount = 0.15 * Math.pow((distance - minPullDistance) / (maxDistance - minPullDistance), 2);
+        const pullX = (deltaX / distance) * (maxDistance - distance) * pullAmount;
+        const pullY = (deltaY / distance) * (maxDistance - distance) * pullAmount;
+
+        setSpring({ transform: `translate(${pullX * 0.8}px, ${pullY * 1.3}px)` });
+        setSpring2({ transform: `translate(${pullX * 1.2}px, ${pullY * 0.8}px)` });
+      } else {
+        setSpring({ transform: 'translate(0px, 0px)' });
+        setSpring2({ transform: 'translate(0px, 0px)' });
+      }
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => document.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+
+  return <>
+    <animated.div className="overlay-button-container">
+      <animated.div ref={aboutButtonRef} style={springProps} onMouseMove={handleMouseMove}  className="overlay-button">
+        <p onClick={openAboutModal}>about</p>
+      </animated.div>
+      <animated.div ref={contactButtonRef} style={springProps2} onMouseMove={handleMouseMove} className="overlay-button">
+        <p onClick={openContactModal}>contact</p>
+      </animated.div>
+    </animated.div>
+
+    {/* about modal slide out */}
+    <animated.div className="about-modal-container" style={animation}>
+      <div className="about-modal" style={pointerEvents}>
+        <div className="closing-button" onClick={openAboutModal} >
+          <p>+</p>
+        </div>
+        <div className="about-modal-header">
+          <h2>About SocialAmp</h2>
+        </div>
+        <div className="about-text">
+          <p>SocialAmp can be used to amplify your movie's social media engagement. We use Twitter and other social media outlets to curate short video clips in response to user comments. Giving fans a deep sense of engagement to the movie they love.</p>
+        </div>
+      </div>
+    </animated.div>
+
+
+    {/* Contact Us Modal */}
+    <animated.div className="contact-modal-container" style={contactAnimation} >
+      <div className="contact-modal" style={contactPointerEvents}>
+      <div className="closing-button" onClick={openContactModal} >
+        <p>+</p>
+      </div>
+        <div className="contact-modal-header">
+          <h2>Contact Us</h2>
+        </div>
+        <div className="contact-entity">
+          <div className="contact-entity-title">
+            <h3>Phone</h3>
+          </div>
+        </div>
+      </div>
+    </animated.div>
+
+  </>
+}
